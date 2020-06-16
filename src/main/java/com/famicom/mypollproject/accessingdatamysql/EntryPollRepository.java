516717b720b1f3f1ff17ad469b99cc4c0ab1493b
Set Israel Ortiz Monasterio <pixrael777@gmail.com>
2020-06-15 22:33:18 -0300

    MPP-43- created post method for the creation of polls

---
+++ src/main/java/com/famicom/mypollproject/accessingdatamysql/EntryPollRepository.java
@@ -0,0 +1,5 @@
+package com.famicom.mypollproject.accessingdatamysql;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EntryPollRepository extends CrudRepository<EntryPoll, Integer> {}
