f57929df2a68663fcae539511539a3f477bf9be3
Set Israel Ortiz Monasterio <pixrael777@gmail.com>
2020-06-20 12:40:27 -0300

    MPP-43- Created api request to save the poll in the database

--- src/main/java/com/famicom/mypollproject/MyPollProjectController.java
+++ src/main/java/com/famicom/mypollproject/MyPollProjectController.java
@@ -1,5 +1,6 @@
 package com.famicom.mypollproject;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.famicom.mypollproject.accessingdatamysql.EntryPoll;
+import com.famicom.mypollproject.accessingdatamysql.EntryPollRepository;
 import com.famicom.mypollproject.accessingdatamysql.Poll;
 import com.famicom.mypollproject.accessingdatamysql.PollRepository;
 
@@ -18,6 +21,9 @@ public class MyPollProjectController {
 
 	@Autowired // This means to get the bean called userRepository
 	private PollRepository pollRepository;
+
+	@Autowired // This means to get the bean called userRepository
+	private EntryPollRepository entryPollRepository;
 
 	@GetMapping(path = "/getAllPolls")
 	public @ResponseBody Iterable<Poll> getAllPolls() {
@@ -28,33 +34,67 @@ public class MyPollProjectController {
 	@PostMapping(path = "/createPoll", consumes = "text/plain") // Map ONLY POST
 																// Requests
 	public @ResponseBody String addNewPoll(@RequestBody String createdPoll) throws Exception {
-		// @ResponseBody means the returned String is the response, not a view
-		// name
-		// @RequestParam means it is a parameter from the GET or POST request
-		
-		JSONParser parser = new JSONParser(); 
+		JSONParser parser = new JSONParser();
 		JSONObject json = (JSONObject) parser.parse(createdPoll);
 
+		Poll poll = new Poll();
 
-		System.out.println("=>: " + createdPoll);
-		System.out.println( "name= " + json.get("name") );
+		poll.setName((String) json.get("name"));
 
-		// for(Integer i = 0;i <cp.getEntries().length;i++){
-		// System.out.println( i + "entry title:" +
-		// cp.getEntries()[i].getTitle() );
-		// System.out.println( i + "entry author:" +
-		// cp.getEntries()[i].getAuthor() );
-		// System.out.println( i + "entry creation date:" +
-		// cp.getEntries()[i].getCreationDate() );
-		// System.out.println( i + "entry img url:" +
-		// cp.getEntries()[i].getImgurl() );
-		// }
+		JSONArray entries = (JSONArray) json.get("entries");
 
-		// User n = new User();
-		// n.setName(name);
-		// n.setEmail(email);
-		// userRepository.save(n);
+		for (int i = 0; i < entries.size(); i++) {
+			JSONObject entry = (JSONObject) entries.get(i);
+			EntryPoll entryPoll = new EntryPoll();
+			entryPoll.setImgurl((String) entry.get("imgurl"));
+			entryPoll.setTitle((String) entry.get("title"));
+			entryPoll.setAuthor((String) entry.get("author"));
+			entryPoll.setCreationDate((String) entry.get("creationDate"));
+
+			EntryPoll savedEntryPoll = this.entryPollRepository.save(entryPoll);
+			
+			this.saveEntryPoll(savedEntryPoll.getId(), i, poll);
+		}
+		
+		pollRepository.save(poll);
+		
 		return "Saved";
+	}
+
+	private void saveEntryPoll(int idEntryPoll, int index, Poll poll) {
+		switch (index) {
+		case 0:
+			poll.setId_poll_entry1(idEntryPoll);
+			break;
+		case 1:
+			poll.setId_poll_entry2(idEntryPoll);
+			break;
+		case 2:
+			poll.setId_poll_entry3(idEntryPoll);
+			break;
+		case 3:
+			poll.setId_poll_entry4(idEntryPoll);
+			break;
+		case 4:
+			poll.setId_poll_entry5(idEntryPoll);
+			break;
+		case 5:
+			poll.setId_poll_entry6(idEntryPoll);
+			break;
+		case 6:
+			poll.setId_poll_entry7(idEntryPoll);
+			break;
+		case 7:
+			poll.setId_poll_entry8(idEntryPoll);
+			break;
+		case 8:
+			poll.setId_poll_entry9(idEntryPoll);
+			break;
+		case 9:
+			poll.setId_poll_entry10(idEntryPoll);
+			break;			
+		}
+
 	}
 
 }
